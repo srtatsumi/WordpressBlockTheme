85715a33d40bcb0f1f5502c3a1163fe486b3e9a4
srtatsumi <soumyadeep1399@gmail.com>
2022-06-17 16:31:56 +0530

    Pet Shop

---
+++ pet-shop-theme/theme-blocks/banner.js
@@ -0,0 +1,14 @@
+wp.blocks.registerBlockType("theme-blocks/banner",{
+    title:"Pet Shop Banner",
+    edit: EditComponent,
+    save: SaveComponent
+})
+
+function EditComponent(){
+    return (
+        <></>
+    )
+}
+function SaveComponent(){
+    return <p>THis is Demo</p>
+}
